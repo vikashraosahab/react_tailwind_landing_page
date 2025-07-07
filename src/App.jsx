2ca63c4beed660cc6070912a498dd749589098bc
vikashraosahab <vikashvinayrao@gmail.com>
2025-07-07 22:44:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+
+export default function App() {
+  return (
+    <Fragment>
+      <h1 className="bg-green-500 font-mono text-center">Hello world</h1>
+    </Fragment>
+  )
+}
+
